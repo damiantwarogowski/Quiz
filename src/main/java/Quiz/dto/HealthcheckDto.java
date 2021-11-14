2ee85d7bff5a636dac95c87e6b107c29543e1e09
DamianT <damian.twarogowski@gmail.com>
2021-11-14 18:25:01 +0100

    14.11.2021

---
+++ src/main/java/Quiz/dto/HealthcheckDto.java
@@ -0,0 +1,11 @@
+package Quiz.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class HealthcheckDto {
+    private boolean status;
+    private String message;
+}
