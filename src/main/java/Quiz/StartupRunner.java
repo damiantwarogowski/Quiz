963cf663a10a3750ae8898fc1eabd00979c82160
DamianT <damian.twarogowski@gmail.com>
2021-11-30 19:44:59 +0100

    25.11.2021

--- src/main/java/Quiz/StartupRunner.java
+++ src/main/java/Quiz/StartupRunner.java
@@ -2,6 +2,7 @@ package Quiz;
 
 import Quiz.database.entities.PlayerEntity;
 import Quiz.database.repositories.PlayerRepository;
+import Quiz.services.QuizDataService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,25 +10,28 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-
 @Component
 @Log
 public class StartupRunner implements CommandLineRunner {
 
     @Autowired
     private PlayerRepository playerRepository;
+    @Autowired
+    private QuizDataService quizDataService;
 
     @Override
-    public void run(String...args) throws Exception {
+    public void run(String... args) throws Exception {
         log.info("Executing startup actions...");
         playerRepository.save(new PlayerEntity("John"));
         playerRepository.save(new PlayerEntity("Harry"));
         playerRepository.save(new PlayerEntity("George"));
+
 
         log.info("List of players from database:");
         List<PlayerEntity> playersFromDatabase = playerRepository.findAll();
         for (PlayerEntity player : playersFromDatabase) {
             log.info("Retrieved player: " + player);
         }
+        quizDataService.getQuizCategories();
     }
 }
